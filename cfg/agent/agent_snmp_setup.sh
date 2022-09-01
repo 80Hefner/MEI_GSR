dbd5351276925ace9247381f1936194007fdcbb7
80Hefner <a89597@alunos.uminho.pt>
2022-09-01 03:06:46 +0100

    apenas o Proxy Agent pode fazer pedidos SNMP ao Agent. o contrário já não é possível

--- cfg/agent/agent_snmp_setup.sh
+++ cfg/agent/agent_snmp_setup.sh
@@ -7,9 +7,8 @@ sudo systemctl enable snmpd
 # backup the default configuration file
 sudo mv /etc/snmp/snmpd.conf /etc/snmp/snmpd.conf.bak
 
-# copy the files to the snmpconfpath directory
+# copy new config to the snmpconfpath directory
 sudo cp snmpd.conf /etc/snmp/
-sudo cp snmp.conf /etc/snmp/
 
 # start snmp proccess
 sudo systemctl start snmpd
