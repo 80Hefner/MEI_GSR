1a5fa32ae17d8e21815bde7a3e15100faccda200
80Hefner <a89597@alunos.uminho.pt>
2022-09-04 03:08:37 +0100

    correcao de bug em que o snmp_requester crashava com um dado input

--- src/snmp_requester.py
+++ src/snmp_requester.py
@@ -8,44 +8,48 @@ from mibsec import MIBSec_TypeArg
 def get_request(target: str, oid: str, credentials: str, port: int = 161,
                 engine: hlapi.SnmpEngine = hlapi.SnmpEngine(),
                 context: hlapi.ContextData = hlapi.ContextData()):
-    handler = hlapi.getCmd(
-        engine,
-        hlapi.CommunityData(credentials),
-        hlapi.UdpTransportTarget((target, port)),
-        context,
-        hlapi.ObjectType(hlapi.ObjectIdentity(oid))
-    )
-    return _fetch_get(handler)
+    try:
+        handler = hlapi.getCmd(
+            engine,
+            hlapi.CommunityData(credentials),
+            hlapi.UdpTransportTarget((target, port)),
+            context,
+            hlapi.ObjectType(hlapi.ObjectIdentity(oid))
+        )
+        return _fetch_get(handler)
+    
+    except Exception as e:
+        return (str(e), MIBSec_TypeArg.STR)    
 
 # Faz um pedido get next para um objeto na MIB, indicando o IP ou nome do dispositivo remoto,
 # o OID requisitado e a Community String
 def get_next_request(target: str, oid: str, credentials: str, port: int = 161,
                         engine: hlapi.SnmpEngine = hlapi.SnmpEngine(),
                         context: hlapi.ContextData = hlapi.ContextData()):
-    handler = hlapi.nextCmd(
-        engine,
-        hlapi.CommunityData(credentials),
-        hlapi.UdpTransportTarget((target, port)),
-        context,
-        hlapi.ObjectType(hlapi.ObjectIdentity(oid))
-    )
-    return _fetch_get(handler)
+    try:
+        handler = hlapi.nextCmd(
+            engine,
+            hlapi.CommunityData(credentials),
+            hlapi.UdpTransportTarget((target, port)),
+            context,
+            hlapi.ObjectType(hlapi.ObjectIdentity(oid))
+        )
+        return _fetch_get(handler)
+    
+    except Exception as e:
+        return (str(e), MIBSec_TypeArg.STR)
 
 # Obtém o primeiro objeto do handler e retorna o resultado do pedido
 # O handler deve ser o resultado de um pedido com apenas um OID
 def _fetch_get(handler) -> Tuple[Any, MIBSec_TypeArg]:
     # Obter primeiro objeto do iterador
-    try:
-        error_indication, error_status, error_index, var_binds = next(handler)
+    error_indication, error_status, error_index, var_binds = next(handler)
 
-        # Verificar se houve erros no pedido get
-        if not error_indication and not error_status:
-            result = _cast(var_binds[0][1])
-        else:
-            result = (f'Got SNMP error \'{error_indication}\'', MIBSec_TypeArg.STR)
-
-    except Exception as e:
-        result = (str(e), MIBSec_TypeArg.STR)
+    # Verificar se houve erros no pedido get
+    if not error_indication and not error_status:
+        result = _cast(var_binds[0][1])
+    else:
+        result = (f'Got SNMP error \'{error_indication}\'', MIBSec_TypeArg.STR)
 
     return result
 
